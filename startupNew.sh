162987d8bf3647449361fa32e8a976366bcee0aa
Samuel Bruce <sbruce1@firstderivatives.com>
2021-08-02 17:44:15 +1000

    Changed startupNew.sh to be more robust and relative to the working directory

--- startupNew.sh
+++ startupNew.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+echo $PWD
+export BASE_CLONE_DIRECTORY=$PWD
+echo $BASE_CLONE_DIRECTORY
 echo "Starting Dashboards"
-cd /home/sbruce1/Desktop/Work/Projects/Tick\ Architecture/dash
+cd $BASE_CLONE_DIRECTORY/dash
 q sample/demo.q -u 1 &
 q dash.q -p 10001 -u 1 &
 echo "Starting Tickerplant" &
-cd /home/sbruce1/developer/workspace/a/Test/kdb-tick
+cd $BASE_CLONE_DIRECTORY/Test/kdb-tick
 q tick.q sym /home/sbruce1/sbruce1/OnDiskDB/TickerPlantTestDB -p 5000 &
 echo "Starting HDB"
-cd /home/sbruce1/developer/workspace/a/Test/kdb-tick/HDB
+cd $BASE_CLONE_DIRECTORY/Test/kdb-tick/HDB
 q hdb.q /home/sbruce1/sbruce1/OnDiskDB/TickerPlantTestDB -p 5002 &
 echo "Starting RTS"
-cd /home/sbruce1/developer/workspace/a/Test/kdb-tick/tick
+cd $BASE_CLONE_DIRECTORY/Test/kdb-tick/tick
 q RealTimeTradeWithAsofQuotes.q -tp localhost:5000 -syms MSFT.O IBM.N GS.N -p 5001 &
 echo "Starting RDB"
 q r.q localhost:5000 localhost:5002 -p 5005 &
