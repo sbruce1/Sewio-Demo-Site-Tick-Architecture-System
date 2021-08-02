5f0de2748b3402ea336139603adbf26ab3ed0c9c
Samuel Bruce <sbruce1@firstderivatives.com>
2021-08-02 16:59:43 +1000

    Changing startupNew.sh to not include developer startup

--- startupNew.sh
+++ startupNew.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-echo "Establishing Function"
-dev()
-{
-	source /home/sbruce1/developer/config/config.profile;q /home/sbruce1/developer/launcher.q_
-}
 echo "Starting Dashboards"
 cd /home/sbruce1/Desktop/Work/Projects/Tick\ Architecture/dash
 q sample/demo.q -u 1 &
@@ -19,5 +14,3 @@ cd /home/sbruce1/developer/workspace/a/Test/kdb-tick/tick
 q RealTimeTradeWithAsofQuotes.q -tp localhost:5000 -syms MSFT.O IBM.N GS.N -p 5001 &
 echo "Starting RDB"
 q r.q localhost:5000 localhost:5002 -p 5005 &
-echo "Starting Developer"
-dev
