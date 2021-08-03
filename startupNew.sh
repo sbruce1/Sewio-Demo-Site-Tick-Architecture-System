990ef2188f111d4a4cceb1b0bcde4931714400b6
Samuel Bruce <sbruce1@firstderivatives.com>
2021-08-03 14:54:02 +1000

    startupNew.sh configured correctly

--- startupNew.sh
+++ startupNew.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo $PWD
-export BASE_CLONE_DIRECTORY=$PWD
+
+# Insert your own directory for ON_DISK_HDB and BASE_CLONE_DIRECTORY here
+ON_DISK_HDB="/home/sbruce1/sbruce1/OnDiskDB/TickerPlantTestDB/"
+BASE_CLONE_DIRECTORY="/home/sbruce1/Desktop/Work/Projects/Git/Sewio/Sewio-Demo-Site-Tick-Architecture-System/"
+
+
 echo $BASE_CLONE_DIRECTORY
 echo "Starting Dashboards"
 cd $BASE_CLONE_DIRECTORY/dash
@@ -8,10 +12,10 @@ q sample/demo.q -u 1 &
 q dash.q -p 10001 -u 1 &
 echo "Starting Tickerplant" &
 cd $BASE_CLONE_DIRECTORY/Test/kdb-tick
-q tick.q sym /home/sbruce1/sbruce1/OnDiskDB/TickerPlantTestDB -p 5000 &
+q tick.q sym $ON_DISK_HDB -p 5000 &
 echo "Starting HDB"
 cd $BASE_CLONE_DIRECTORY/Test/kdb-tick/HDB
-q hdb.q /home/sbruce1/sbruce1/OnDiskDB/TickerPlantTestDB -p 5002 &
+q hdb.q $ON_DISK_HDB -p 5002 &
 echo "Starting RTS"
 cd $BASE_CLONE_DIRECTORY/Test/kdb-tick/tick
 q RealTimeTradeWithAsofQuotes.q -tp localhost:5000 -syms MSFT.O IBM.N GS.N -p 5001 &
